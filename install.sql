c07819a117d8439952e503b1f4fa1fc0fb0a3092
David Neustadt <saberdude@googlemail.com>
2018-07-27 16:49:33 +0200

    users table structure

--- install.sql
+++ install.sql
@@ -2,13 +2,12 @@ CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` text NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `verified` tinyint(1) NOT NULL DEFAULT '0',
   `admin` tinyint(1) DEFAULT '0',
   `sessionID` text,
   `created` datetime DEFAULT NULL,
   `lastLogin` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
