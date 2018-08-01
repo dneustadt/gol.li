19c7bcb5493a848c9b467ffa812e290ebdcdbdcb
David Neustadt <saberdude@googlemail.com>
2018-08-01 19:36:03 +0200

    added favicons variations

--- Views/skeleton.php
+++ Views/skeleton.php
@@ -6,22 +6,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="<?= @$data['base_path']; ?>/web/css/dist/all.min.css">
         <link rel="shortcut icon" type="image/x-icon" href="<?= @$data['base_path']; ?>/favicon.ico">
-        <link rel="apple-touch-icon" sizes="57x57" href="<?= @$data['base_path']; ?>/web/img/favicon/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="<?= @$data['base_path']; ?>/web/img/favicon/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="<?= @$data['base_path']; ?>/web/img/favicon/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="<?= @$data['base_path']; ?>/web/img/favicon/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="<?= @$data['base_path']; ?>/web/img/favicon/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="<?= @$data['base_path']; ?>/web/img/favicon/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="<?= @$data['base_path']; ?>/web/img/favicon/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="<?= @$data['base_path']; ?>/web/img/favicon/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="<?= @$data['base_path']; ?>/web/img/favicon/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="<?= @$data['base_path']; ?>/web/img/favicon/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="<?= @$data['base_path']; ?>/web/img/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="<?= @$data['base_path']; ?>/web/img/favicon/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="<?= @$data['base_path']; ?>/web/img/favicon/favicon-16x16.png">
-        <link rel="manifest" href="<?= @$data['base_path']; ?>/web/img/favicon/manifest.json">
+        <link rel="apple-touch-icon" sizes="57x57" href="<?= @$data['base_path']; ?>/web/img/favicons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?= @$data['base_path']; ?>/web/img/favicons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= @$data['base_path']; ?>/web/img/favicons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?= @$data['base_path']; ?>/web/img/favicons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= @$data['base_path']; ?>/web/img/favicons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?= @$data['base_path']; ?>/web/img/favicons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?= @$data['base_path']; ?>/web/img/favicons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?= @$data['base_path']; ?>/web/img/favicons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= @$data['base_path']; ?>/web/img/favicons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="<?= @$data['base_path']; ?>/web/img/favicons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?= @$data['base_path']; ?>/web/img/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="<?= @$data['base_path']; ?>/web/img/favicons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?= @$data['base_path']; ?>/web/img/favicons/favicon-16x16.png">
+        <link rel="manifest" href="<?= @$data['base_path']; ?>/web/img/favicons/manifest.json">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="<?= @$data['base_path']; ?>/web/img/favicon/ms-icon-144x144.png">
+        <meta name="msapplication-TileImage" content="<?= @$data['base_path']; ?>/web/img/favicons/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
     </head>
     <body class="<?= @$data['controller']; ?><?php if (!@$data['is_loggedin']): ?> logged-out<?php endif; ?>">
