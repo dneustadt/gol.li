28960c0c8b5695baa4b8fb85510fa42074c12cdb
neustadt <dneustadt@webmatch.de>
2018-08-07 00:50:16 +0200

    password reset forms layout

--- Views/login/password.php
+++ Views/login/password.php
@@ -5,29 +5,34 @@
         with your new credentials.
     </p>
 <?php else: ?>
-    <fieldset class="reset-form">
-        <legend>Set new password:</legend>
-        <form action="<?= @$data['base_path']; ?>/login/password" method="post">
-            <div class="reset-form--field">
-                <input placeholder="Password" type="password" minlength="6" id="password" name="_password" required>
-            </div>
-            <?php if (@$data['error']['password']): ?>
-                <ul class="errors">
-                    <li>Must at least contain 6 characters</li>
-                </ul>
-            <?php endif; ?>
-            <div class="reset-form--field">
-                <input placeholder="Confirm Password" type="password" minlength="6" id="password_confirm" name="_password_confirm" required>
-            </div>
-            <?php if (@$data['error']['password_confirm']): ?>
-                <ul class="errors">
-                    <li>The provided passwords didn't match</li>
-                </ul>
-            <?php endif; ?>
-            <input type="hidden" name="_token" value="<?= @$data['token']; ?>">
-            <div class="reset-form--field">
-                <button type="submit">Set New Password</button>
-            </div>
-        </form>
-    </fieldset>
+    <div class="row">
+        <div class="column">
+            <fieldset class="reset-form">
+                <legend>Set new password:</legend>
+                <form action="<?= @$data['base_path']; ?>/login/password" method="post">
+                    <div class="reset-form--field">
+                        <input placeholder="Password" type="password" minlength="6" id="password" name="_password" required>
+                    </div>
+                    <?php if (@$data['error']['password']): ?>
+                        <ul class="errors">
+                            <li>Must at least contain 6 characters</li>
+                        </ul>
+                    <?php endif; ?>
+                    <div class="reset-form--field">
+                        <input placeholder="Confirm Password" type="password" minlength="6" id="password_confirm" name="_password_confirm" required>
+                    </div>
+                    <?php if (@$data['error']['password_confirm']): ?>
+                        <ul class="errors">
+                            <li>The provided passwords didn't match</li>
+                        </ul>
+                    <?php endif; ?>
+                    <input type="hidden" name="_token" value="<?= @$data['token']; ?>">
+                    <div class="reset-form--field">
+                        <button type="submit">Set New Password</button>
+                    </div>
+                </form>
+            </fieldset>
+        </div>
+        <div class="column"></div>
+    </div>
 <?php endif; ?>
--- Views/login/reset.php
+++ Views/login/reset.php
@@ -4,15 +4,20 @@
         including a link to reset your password.
     </p>
 <?php else: ?>
-    <fieldset class="reset-form">
-        <legend>Request password reset:</legend>
-        <form action="<?= @$data['base_path']; ?>/login/reset" method="post">
-            <div class="reset-form--field">
-                <input placeholder="Email" type="email" id="email" name="_email" required>
-            </div>
-            <div class="reset-form--field">
-                <button type="submit">Request Password Reset</button>
-            </div>
-        </form>
-    </fieldset>
+    <div class="row">
+        <div class="column">
+            <fieldset class="reset-form">
+                <legend>Request password reset:</legend>
+                <form action="<?= @$data['base_path']; ?>/login/reset" method="post">
+                    <div class="reset-form--field">
+                        <input placeholder="Email" type="email" id="email" name="_email" required>
+                    </div>
+                    <div class="reset-form--field">
+                        <button type="submit">Request Password Reset</button>
+                    </div>
+                </form>
+            </fieldset>
+        </div>
+        <div class="column"></div>
+    </div>
 <?php endif; ?>
